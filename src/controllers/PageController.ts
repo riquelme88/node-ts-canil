529bd0ca6250d09d77c6868180441dee74ed545b
Riquelme <riquelmestayler@gmail.com>
2024-01-18 12:20:09 -0300

    base criada

---
+++ src/controllers/PageController.ts
@@ -0,0 +1,17 @@
+import { Request,Response } from "express";
+
+export const home = (req : Request, res:Response) => {
+     res.render('pages/page')
+}
+
+export const dog = (req: Request, res:Response) =>{
+
+}
+
+export const cat = (req: Request, res:Response) =>{
+    
+}
+
+export const fish = (req: Request, res:Response) =>{
+    
+}   
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,25 @@
-import  Express  from "express";
+import express from "express";
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import router from "./routes";
+
+dotenv.config();
+
+const server = express()
+
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname,'views'))
+server.engine('mustache', mustache())
+
+server.use(express.static(path.join(__dirname, '../public')))
+
+//Rotas
+
+server.use(router)
+
+server.use((req,res)=>{
+    res.send("Nada encontrado")
+})
+
+server.listen(process.env.PORT)
